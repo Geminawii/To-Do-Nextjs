ac55f6dbbca7f1a40346663bbcea20cd9b777231
Omona Coker <teminirecokef@gmail.com>
2025-09-25 22:41:17 +0100

    Restructured project and moved files to root

--- justdoeet/src/components/ErrorCheck.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ErrorCheck: React.FC = () => {
-  throw new Error("Trigger error boundary");
-};
-
-export default ErrorCheck;
--- justdoeet/src/types/todo.ts
+++
@@ -1,9 +0,0 @@
-export interface Todo {
-  id: number | string;
-  todo: string;
-  completed: boolean;
-  userId: number | string;
-  priority?: "high" | "low" | "medium";
-  date?: string;
-  description?: string;
-}
